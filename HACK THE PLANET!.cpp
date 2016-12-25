c9e4ca6aa4368ffb167f8e1cee51f1888587e3b9
Ronaldoox <ronaldo19302yt@gmail.com>
2016-12-25 20:11:17 -0300

    Add files via upload

---
+++ HACK THE PLANET!.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstdlib>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+system("color a");
+cout<<"            HACK THE PLANET!"<<endl<<endl;
+cout<<"             $$$$$$$$$$$$$b"<<endl;            
+cout<<"            $$$$$$$$$$$$$$$$"<<endl;         
+cout<<"           4$$$$$$$$$$$$$$$$F"<<endl;        
+cout<<"           4$$$$$$$$$$$$$$$$F"<<endl;        
+cout<<"          $$$$”  ”$$$$”  ”$$$$"<<endl;          
+cout<<"          $F      4$$F      4$”"<<endl;          
+cout<<"          $$      $$$$      $P"<<endl;            
+cout<<"             4$$$$$”^$$$$$%"<<endl;          
+cout<<"              ”$$$$$$$$$$$"<<endl;               
+cout<<"               *$$$$$$$$"<<endl;                 
+cout<<"  4$$                              $$r"<<endl;
+cout<<"  $$$b                            e$$$”"<<endl;  
+cout<<"  d$$$$$e                    z$$$$$b"<<endl;  
+cout<<"    4$$$*$$$$$c        .$$$$$*$$$r"<<endl;
+cout<<"             *$$$be$$$*”"<<endl;        
+cout<<"                ”$$$$$$”"<<endl;                    
+cout<<"               .d$$P$$$b"<<endl;                  
+cout<<"             d$$P      ^$$$b"<<endl;              
+cout<<"     .ed$$$”              ”$$$be."<<endl;      
+cout<<" $$$$$$P                    *$$$$$$"<<endl;
+cout<<"4$$$$$P                        $$$$$$"<<endl;
+cout<<" $$$          	                 ^$$P"<<endl;
+
+cout<<"\n";
+cout<<"\nby zCyberGhost.exe";
+cout<<"\n";
+	getch();
+	system("pause");
+	return 0;
+}
+
